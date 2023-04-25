cae23eb8bb44473115fe445732b235d6ffd9cb10
lga7799 <44216089+lga7799@users.noreply.github.com>
2023-04-25 17:07:46 -0500

    Fixed Decoder and added new instructions to conv

--- decoder_unit.v
+++ decoder_unit.v
@@ -17,8 +17,8 @@ module decoder_block(
     output           vl_load,
 );
     // wire [];
-    case (cmd_payload_function_id[9:5])
-        5'h17     ://vsetvli
+    case (cmd_payload_function_id[2:0])
+        3'h0     ://vsetvli
             vl_load=1;
             bus_sel=2bXX;
             reg_op0_sel=5hXX;
@@ -28,53 +28,53 @@ module decoder_block(
             alu_op1_sel=X;
             alu_mode=2bXX;
             reg_load=0;
-        5'h07     ://vload
+        3'h1     ://vload
             vl_load=0;
             reg_load=cmd_valid;
             bus_sel=2b00;//Take care of in core file
-            reg_wb_sel=cmd_payload_function_id[4:0];
+            reg_wb_sel=cmd_payload_function_id[7:3];
             reg_op0_sel=5hXX;
             reg_op1_sel=5hXX;
             alu_imm=8hXX;
             alu_op1_sel=X;
             alu_mode=2bXX;
-        5'h15     ://vadd.vector - immediate
+        3'h2     ://vadd.vector - immediate
             vl_load=0;
             reg_load=cmd_valid;
             bus_sel=2b01;
-            reg_wb_sel=cmd_payload_function_id[4:0];
+            reg_wb_sel=cmd_payload_function_id[7:3];
             reg_op0_sel=cmd_payload_inputs_0[4:0];
             reg_op1_sel=8hXX;
-            alu_imm=cmd_payload_inputs_1[7:0];
+            alu_imm=cmd_payload_inputs_0[7:0];
             alu_op1_sel=1;
             alu_mode=2bXX;
 
-        5'h0D     ://vacc - group add
+        3'h3     ://vacc - group add
             vl_load=0;
             reg_load=cmd_valid;
             bus_sel=00;
-            reg_wb_sel=cmd_payload_function_id[4:0];
+            reg_wb_sel=cmd_payload_function_id[7:3];
             reg_op0_sel=cmd_payload_inputs_0[4:0];
             reg_op1_sel=5hXX;
             alu_imm=8hXX;
             alu_op1_sel=X;
             alu_mode=2bXX;
-        5'h04     ://vmul
+        3'h4     ://vmul
             vl_load=0;
             reg_load=cmd_valid;
             bus_sel=2b10;
-            reg_wb_sel=cmd_payload_function_id[4:0];
+            reg_wb_sel=cmd_payload_function_id[7:3];
             reg_op0_sel=cmd_payload_inputs_0[4:0];
             reg_op1_sel=cmd_payload_inputs_1[4:0];
             alu_imm=8hXX;
             alu_op1_sel=X;
             alu_mode=2bXX;
-        5'h1D     ://vbacc - byte add
+        3'h5     ://vbacc - byte add
             vl_load=0;
             reg_load=0;
             bus_sel=2b11;
-            reg_wb_sel=cmd_payload_function_id[4:0];
-            reg_op0_sel=5hXX;
+            reg_wb_sel=5hXX;
+            reg_op0_sel=cmd_payload_function_id[7:3];
             reg_op1_sel=5hXX;
             alu_imm=8hXX;
             alu_op1_sel=X;
